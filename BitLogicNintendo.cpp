b2a6d304b31f5ea772ecbdc29262b7b8aa1c6845
DFI <dfi@dfidaali.com>
2015-07-01 20:33:32 +0200

    nice happy show. Hooo, so excited !

--- BitLogicNintendo.cpp
+++ BitLogicNintendo.cpp
@@ -9,6 +9,7 @@
 #include "Tools.h"
 #include <iomanip>
 #include <bitset>
+#include <vector>
 
 using namespace std;
 
@@ -18,7 +19,20 @@ using namespace std;
  * "00000083 000000e5"
  */
 
-
+ std::vector<bitset<512>> solveIt(bitset<512> input,bitset<512> hypoth,int size,int depth){
+    std::vector<bitset<512>> res(128);
+    int magic[]={0,size/2-1,size/2+1,size-1};
+    for(int i=0;i<4;i++){
+        magic[i]+=depth;        
+    }
+    for(unsigned int i=0;i<16;i++){
+        bitset<512> v;
+        v[magic[0]]=i&1;
+        v[magic[1]]=(i>>1 & 1);
+    }
+    
+    return res;
+}
 
  std::string toStringBs(bitset<512> dat,int size){          
       std::string mystring =
@@ -48,26 +62,44 @@ using namespace std;
      
      return res;
  }
-
-int main(int argc, char** argv) {   
+ 
+ void showSomeDisplayVariableSize(){
     testInputOutputCompareRef();
     
     bitset<512> my;
-        int size=8;
+        int size=6;
       
-    cout << formula(size/2) << endl;         
-    cout << formula(size) << endl;     
-    cout << formula(size*2) << endl;
-    for(int i=0;i<0xFF;i++){
-        my=i;
-        
-        cout<< toStringBs(my,size) << " -> ";
-        cout<< toStringBs(applyDirectFun(my,size),size)<<endl;        
-    }
+    //cout << formula(size/2) << endl;         
+    //cout << formula(size) << endl;     
+    //cout << formula(size*2) << endl;
+    
+    for(int sz=0;sz<=size;sz++){
+        cout << formula(sz) << endl;    
+        for(int i=0;i<myPow(2,sz);i++){
+            my=i;
+
+            cout<< toStringBs(my,sz) << " -> ";
+            cout<< toStringBs(applyDirectFun(my,sz),sz)<<endl;        
+        }
     
 
+    }
     
+     
+ 
+ }
+
+int main(int argc, char** argv) {   
+    bitset<512> my;
+        int size=16;
+      
+    //cout << formula(size/2) << endl;         
+    //cout << formula(size) << endl;     
+    //cout << formula(size*2) << endl;
     
+    for(int sz=16;sz<=size;sz++){
+        cout << formula(sz) << endl;       
+    }
 
     return 0;
 }
--- BlN_ext001.h
+++ BlN_ext001.h
@@ -9,6 +9,17 @@
 #define	BLN_EXT001_H
 
 
+int myPow(int x, int p)
+{
+  if (p == 0) return 1;
+  if (p == 1) return x;
+
+  int tmp = myPow(x, p/2);
+  if (p%2 == 0) return tmp * tmp;
+  else return x * tmp * tmp;
+}
+
+
 bitset<512> applyDirectFun (bitset<512> dat, int size){
     bitset<512> out;
     for(int i=0;i<size/2;i++){
