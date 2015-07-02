ecdb4e520c55bd38bd7cd51a0e79f562466255dc
DFI <dfi@dfidaali.com>
2015-07-02 15:41:15 +0200

    32 bits okay, fast ! Yeah !!

--- BitLogicNintendo2.cpp
+++ BitLogicNintendo2.cpp
@@ -8,51 +8,8 @@
 #include "BitLogicNintendo2.h"
 
 
-bool increment(int& depth,bitset < SZVEC >& v , int size){
-    
-    int magic[] = {0, size / 2 - 1, size / 2, size - 1};
 
-    bool debugIncr = false; 
-
-            bool foundIncr = false;
-            while (!foundIncr) {
-                if(depth<0) return true;
-                // incr
-                if (depth < (size / 4)) {
-                    {
-                        if (debugIncr) cout << " Magic for d " << depth << " is " << (magic[0] + depth) << "|" << (magic[1] - depth) << "|" << (magic[2] + depth) << "|" << (magic[3] - depth) << "|" << endl;
-                        if (debugIncr) cout << "(K) increment (before)" << " depth is  " << depth << " v " << toStringBs(v, size) << endl;
-
-                        int inc = 0;
-                        inc |= v[magic[0] + depth] ? 1 : 0;
-                        inc |= v[magic[1] - depth] ? 2 : 0;
-                        inc |= v[magic[2] + depth] ? 4 : 0;
-                        inc |= v[magic[3] - depth] ? 8 : 0;
-
-                        inc++;
-                        // Incrementation might be 0 if overflow
-                        v[magic[0] + depth] = inc & 1;
-                        v[magic[1] - depth] = ((inc >> 1) & 1);
-                        v[magic[2] + depth] = ((inc >> 2) & 1);
-                        v[magic[3] - depth] = ((inc >> 3) & 1);
-
-                        if (debugIncr) cout << "(K) increment (after)" << " depth is  " << depth << " v " << toStringBs(v, size) << " inc compt " << inc << endl;
-
-                        if (inc >= 16) {
-                            // Depth end MAX REACH
-                            depth--;
-                        } else { // incr reussie
-                            break;
-                        }
-                    } 
-                }else{ // increMaxReached
-                    depth--;
-                }
-            }    
-            return false;
-}
-
-std::unordered_set<bitset < SZVEC >> solveItV2(bitset<SZVEC> input, int size) {
+std::unordered_set<bitset < SZVEC >> solveItV3(bitset<SZVEC> input, int size) {
     std::unordered_set<bitset < SZVEC >> res;
     bitset<SZVEC> v;
     int magic[] = {0, size / 2 - 1, size / 2, size - 1};
@@ -62,17 +19,17 @@ std::unordered_set<bitset < SZVEC >> solveItV2(bitset<SZVEC> input, int size) {
     bool debugIncr = false;
     bool debugCurrLoopState = false;
     
-    std::unordered_set<bitset < SZVEC >> debugHypSeq[10];
+    //std::unordered_set<bitset < SZVEC >> debugHypSeq[10];
 
     while (true) {
         if(debugCurrLoopState) cout << "dept " << depth << " v " <<  toStringBs(v, size) << endl;
         
-        if (debugHypSeq[depth].find(v) != debugHypSeq[depth].end()) {      
-            cout << " ======= ERROR " << " already visited "<< "for " << depth << endl;
-                        return res;
-        }  else{
-            debugHypSeq[depth].insert(v);
-        }
+        //if (debugHypSeq[depth].find(v) != debugHypSeq[depth].end()) {      
+        //    cout << " ======= ERROR " << " already visited "<< "for " << depth << endl;
+        //                return res;
+        //}  else{
+        //    debugHypSeq[depth].insert(v);
+        //}
 
         // ++++++++++++++++ Left formula on current Hyp
         //+++++++++++++
@@ -122,103 +79,6 @@ std::unordered_set<bitset < SZVEC >> solveItV2(bitset<SZVEC> input, int size) {
 
 }
 
-std::unordered_set<bitset < SZVEC >> solveIt(bitset<SZVEC> input, bitset<SZVEC> hypoth, int size, int depth) {
-    std::unordered_set<bitset < SZVEC >> res;
-    int magic[] = {0, size / 2 - 1, size / 2, size - 1};
-
-    bool activCond = false;
-    bool activate_formul_debug = false;
-    unsigned long valHypActDebug = 41;
-
-    if (activCond)if (depth == 0) activate_formul_debug = true;
-
-    if (activate_formul_debug)
-        cout << "hyp " << toStringBs(hypoth, size) << " deep= " << depth << " ";
-    //  cout <<"magic ";for(int i=0;i<4;i++)cout << magic[i] ;cout << endl;
-    //if( input!=40 || depth==0 )cout <<"input " <<toStringBs(input, size) << "--- depth " << depth << endl;
-
-
-    for (unsigned int i = 0; i < 16; i++) {
-        bitset<SZVEC> v;
-        v[magic[0] + depth] = i & 1;
-        v[magic[1] - depth] = ((i >> 1) & 1);
-        v[magic[2] + depth] = ((i >> 2) & 1);
-        v[magic[3] - depth] = ((i >> 3) & 1);
-
-        //  cout<< "v(b hyp) : " << toStringBs(v, size) << endl;
-
-        for (int i = 0; i < depth; i++) {
-            v[magic[0] + i] = hypoth[magic[0] + i];
-            v[magic[1] - i] = hypoth[magic[1] - i];
-            v[magic[2] + i] = hypoth[magic[2] + i];
-            v[magic[3] - i] = hypoth[magic[3] - i];
-        }
-
-        if (activCond) if (depth >= 1 && v == valHypActDebug) activate_formul_debug = true;
-        if (activate_formul_debug)
-            cout << "--- depth " << depth << " v(aft hyp) : " << toStringBs(v, size) << endl;
-
-        // check formula .... left
-        bool ext = input[0 + depth];
-        bool possible = false;
-        if (activate_formul_debug) cout << "{";
-        for (int i = 0; i < depth + 1; i++) {
-            int h = size / 2;
-            int riand = i;
-            int rilef = h + (depth - i);
-            possible = possible^(v[riand] & v[rilef]);
-            if (activate_formul_debug) cout << "(" << (riand) << "&" << (rilef) << ")" << v[riand] << "&" << v[rilef];
-        }
-        if (activate_formul_debug) cout << "}" << endl;
-        // cout << "ext " << ext << " calc "<< possible << endl;
-        bool rightFormulaIsPassed = false;
-
-        if (depth >= (size / 2) - 1) {
-            rightFormulaIsPassed = (ext == possible);
-        } else
-            if (ext == possible) {
-            // check formula .... right
-            if (activate_formul_debug) cout << " check right formula part inputIs(" << toStringBs(input, size) << ") " << endl;
-            int rightEdge = size - 1;
-            ext = input[size - 2 - depth]; // cout << "Extern bit " << " (" <<(size-2-depth) <<") is " << input[size-2-depth] <<endl;
-            possible = false;
-            if (activate_formul_debug) cout << "right {";
-            for (int i = 0; i < depth + 1; i++) {
-                int h = size / 2;
-                int riand = h - i - 1;
-                int rilef = rightEdge - depth + i;
-                possible = possible^(v[riand] & v[rilef]);
-                if (activate_formul_debug) cout << "(" << (riand) << "&" << (rilef) << ")" << v[riand] << "&" << v[rilef];
-            }
-            if (activate_formul_debug) cout << "}" << endl;
-
-            // cout << "right form " <<" ext "<< ext << " possible "  << possible << endl;
-
-            rightFormulaIsPassed = (ext == possible);
-            //  cout << "righ formula is passed " << rightFormulaIsPassed << endl;
-        }
-
-
-        if (rightFormulaIsPassed) {
-
-            if (depth >= (size / 2) - 1) {
-                //   cout << " adding validated " <<  toStringBs(v, size) << endl;
-
-                res.insert(v);
-
-            } else {
-                std::unordered_set<bitset < SZVEC >> sub = solveIt(input, v, size, depth + 1);
-                if (activate_formul_debug) cout << "++++++++ back to " << depth << " " << "hyp " << toStringBs(hypoth, size) << " deep= " << depth << " " << endl;
-                for (const auto& subv : sub) {
-                    res.insert(subv);
-                    /* ... process elem ... */
-                }
-            }
-        }
-    } // fin parcours hypotheses
-
-    return res;
-}
 
 void testAndValidateMyInvertingAtRandomWithManualSize() {
 
@@ -228,13 +88,13 @@ void testAndValidateMyInvertingAtRandomWithManualSize() {
     std::unordered_set<bitset < SZVEC >> done;
     srand(0x478754);
     bitset<SZVEC> my;
-    int sz = 16;
-    unsigned int val = 0x0e24;
+    int sz = 32;
+    unsigned int val = 0x0e24FF;
     //cout << formula(size/2) << endl;         
     //cout << formula(size) << endl;     
     //cout << formula(size*2) << endl;
 
-    int nbNumberToTry = 1000;
+    int nbNumberToTry = 3;
     long maxValue = 1;
     for (int i = 0; i < sz - 1; i++) {
         maxValue |= maxValue << 1;
@@ -257,7 +117,7 @@ void testAndValidateMyInvertingAtRandomWithManualSize() {
             cout << " from " << toStringBs(myori, sz) << " inverting " <<toStringBs(my, sz)<< endl;
             clock_t begin = clock();
             //std::unordered_set<bitset < SZVEC >> res = solveIt(my,my, sz,0);
-            std::unordered_set<bitset < SZVEC >> res = solveItV2(my, sz);
+            std::unordered_set<bitset < SZVEC >> res = solveItV3(my, sz);
             {
                 int elemind = 0;
                 if (showResult) for (const auto& elem : res) {
--- BitLogicNintendo2.h
+++ BitLogicNintendo2.h
@@ -185,5 +185,223 @@ std::string toStringBs(bitset<SZVEC> dat, int size) {
     return mystring.substr(SZVEC - size, SZVEC);
 } 
 
+
+
+
+
+
+bool increment(int& depth,bitset < SZVEC >& v , int size){
+    
+    int magic[] = {0, size / 2 - 1, size / 2, size - 1};
+
+    bool debugIncr = false; 
+
+            bool foundIncr = false;
+            while (!foundIncr) {
+                if(depth<0) return true;
+                // incr
+                if (depth < (size / 4)) {
+                    {
+                        if (debugIncr) cout << " Magic for d " << depth << " is " << (magic[0] + depth) << "|" << (magic[1] - depth) << "|" << (magic[2] + depth) << "|" << (magic[3] - depth) << "|" << endl;
+                        if (debugIncr) cout << "(K) increment (before)" << " depth is  " << depth << " v " << toStringBs(v, size) << endl;
+
+                        int inc = 0;
+                        inc |= v[magic[0] + depth] ? 1 : 0;
+                        inc |= v[magic[1] - depth] ? 2 : 0;
+                        inc |= v[magic[2] + depth] ? 4 : 0;
+                        inc |= v[magic[3] - depth] ? 8 : 0;
+
+                        inc++;
+                        // Incrementation might be 0 if overflow
+                        v[magic[0] + depth] = inc & 1;
+                        v[magic[1] - depth] = ((inc >> 1) & 1);
+                        v[magic[2] + depth] = ((inc >> 2) & 1);
+                        v[magic[3] - depth] = ((inc >> 3) & 1);
+
+                        if (debugIncr) cout << "(K) increment (after)" << " depth is  " << depth << " v " << toStringBs(v, size) << " inc compt " << inc << endl;
+
+                        if (inc >= 16) {
+                            // Depth end MAX REACH
+                            depth--;
+                        } else { // incr reussie
+                            break;
+                        }
+                    } 
+                }else{ // increMaxReached
+                    depth--;
+                }
+            }    
+            return false;
+}
+
+
+std::unordered_set<bitset < SZVEC >> solveItV2(bitset<SZVEC> input, int size) {
+    std::unordered_set<bitset < SZVEC >> res;
+    bitset<SZVEC> v;
+    int magic[] = {0, size / 2 - 1, size / 2, size - 1};
+
+    int depth = 0;
+
+    bool debugIncr = false;
+    bool debugCurrLoopState = false;
+    
+    std::unordered_set<bitset < SZVEC >> debugHypSeq[10];
+
+    while (true) {
+        if(debugCurrLoopState) cout << "dept " << depth << " v " <<  toStringBs(v, size) << endl;
+        
+        if (debugHypSeq[depth].find(v) != debugHypSeq[depth].end()) {      
+            cout << " ======= ERROR " << " already visited "<< "for " << depth << endl;
+                        return res;
+        }  else{
+            debugHypSeq[depth].insert(v);
+        }
+
+        // ++++++++++++++++ Left formula on current Hyp
+        //+++++++++++++
+        // check formula .... left
+        bool ext = input[0 + depth];
+        bool possible = false;
+        for (int i = 0; i < depth + 1; i++) {
+            int h = size / 2;
+            int riand = i;
+            int rilef = h + (depth - i);
+            possible = possible^(v[riand] & v[rilef]);
+        }
+        if (possible != ext) // reject value
+        {
+            if(increment(depth,v,size)) return res;
+            continue;
+        }
+        if (depth >= (size / 2) -1) {
+            res.insert(v);
+            if(increment(depth,v,size)) return res;
+        }     
+        // ++++++++++++++++++++ Right formula on current Hyp
+        //+++++++++++++++
+        // check formula .... right
+        int rightEdge = size - 1;
+        ext = input[size - 2 - depth]; // cout << "Extern bit " << " (" <<(size-2-depth) <<") is " << input[size-2-depth] <<endl;
+        possible = false;
+        for (int i = 0; i < depth + 1; i++) {
+            int h = size / 2;
+            int riand = h - i - 1;
+            int rilef = rightEdge - depth + i;
+            possible = possible^(v[riand] & v[rilef]);
+        }
+        if (possible != ext) // reject value
+        {
+            if(increment(depth,v,size)) return res;
+            continue;
+        }
+        //+++++++++++++++ Value accepted
+        { // send it to next wave of checks
+            depth++;
+        }
+
+
+
+    }// Fin boucle principale
+
+}
+
+std::unordered_set<bitset < SZVEC >> solveIt(bitset<SZVEC> input, bitset<SZVEC> hypoth, int size, int depth) {
+    std::unordered_set<bitset < SZVEC >> res;
+    int magic[] = {0, size / 2 - 1, size / 2, size - 1};
+
+    bool activCond = false;
+    bool activate_formul_debug = false;
+    unsigned long valHypActDebug = 41;
+
+    if (activCond)if (depth == 0) activate_formul_debug = true;
+
+    if (activate_formul_debug)
+        cout << "hyp " << toStringBs(hypoth, size) << " deep= " << depth << " ";
+    //  cout <<"magic ";for(int i=0;i<4;i++)cout << magic[i] ;cout << endl;
+    //if( input!=40 || depth==0 )cout <<"input " <<toStringBs(input, size) << "--- depth " << depth << endl;
+
+
+    for (unsigned int i = 0; i < 16; i++) {
+        bitset<SZVEC> v;
+        v[magic[0] + depth] = i & 1;
+        v[magic[1] - depth] = ((i >> 1) & 1);
+        v[magic[2] + depth] = ((i >> 2) & 1);
+        v[magic[3] - depth] = ((i >> 3) & 1);
+
+        //  cout<< "v(b hyp) : " << toStringBs(v, size) << endl;
+
+        for (int i = 0; i < depth; i++) {
+            v[magic[0] + i] = hypoth[magic[0] + i];
+            v[magic[1] - i] = hypoth[magic[1] - i];
+            v[magic[2] + i] = hypoth[magic[2] + i];
+            v[magic[3] - i] = hypoth[magic[3] - i];
+        }
+
+        if (activCond) if (depth >= 1 && v == valHypActDebug) activate_formul_debug = true;
+        if (activate_formul_debug)
+            cout << "--- depth " << depth << " v(aft hyp) : " << toStringBs(v, size) << endl;
+
+        // check formula .... left
+        bool ext = input[0 + depth];
+        bool possible = false;
+        if (activate_formul_debug) cout << "{";
+        for (int i = 0; i < depth + 1; i++) {
+            int h = size / 2;
+            int riand = i;
+            int rilef = h + (depth - i);
+            possible = possible^(v[riand] & v[rilef]);
+            if (activate_formul_debug) cout << "(" << (riand) << "&" << (rilef) << ")" << v[riand] << "&" << v[rilef];
+        }
+        if (activate_formul_debug) cout << "}" << endl;
+        // cout << "ext " << ext << " calc "<< possible << endl;
+        bool rightFormulaIsPassed = false;
+
+        if (depth >= (size / 2) - 1) {
+            rightFormulaIsPassed = (ext == possible);
+        } else
+            if (ext == possible) {
+            // check formula .... right
+            if (activate_formul_debug) cout << " check right formula part inputIs(" << toStringBs(input, size) << ") " << endl;
+            int rightEdge = size - 1;
+            ext = input[size - 2 - depth]; // cout << "Extern bit " << " (" <<(size-2-depth) <<") is " << input[size-2-depth] <<endl;
+            possible = false;
+            if (activate_formul_debug) cout << "right {";
+            for (int i = 0; i < depth + 1; i++) {
+                int h = size / 2;
+                int riand = h - i - 1;
+                int rilef = rightEdge - depth + i;
+                possible = possible^(v[riand] & v[rilef]);
+                if (activate_formul_debug) cout << "(" << (riand) << "&" << (rilef) << ")" << v[riand] << "&" << v[rilef];
+            }
+            if (activate_formul_debug) cout << "}" << endl;
+
+            // cout << "right form " <<" ext "<< ext << " possible "  << possible << endl;
+
+            rightFormulaIsPassed = (ext == possible);
+            //  cout << "righ formula is passed " << rightFormulaIsPassed << endl;
+        }
+
+
+        if (rightFormulaIsPassed) {
+
+            if (depth >= (size / 2) - 1) {
+                //   cout << " adding validated " <<  toStringBs(v, size) << endl;
+
+                res.insert(v);
+
+            } else {
+                std::unordered_set<bitset < SZVEC >> sub = solveIt(input, v, size, depth + 1);
+                if (activate_formul_debug) cout << "++++++++ back to " << depth << " " << "hyp " << toStringBs(hypoth, size) << " deep= " << depth << " " << endl;
+                for (const auto& subv : sub) {
+                    res.insert(subv);
+                    /* ... process elem ... */
+                }
+            }
+        }
+    } // fin parcours hypotheses
+
+    return res;
+}
+
 #endif	/* BITLOGICNINTENDO2_H */
 
