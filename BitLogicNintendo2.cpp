e18858c8318f64dfce3d808980ed730035f0d6a0
DFI <dfi@dfidaali.com>
2015-07-02 12:28:55 +0200

    Preparation V2 ... working test cas.

--- BitLogicNintendo2.cpp
+++ BitLogicNintendo2.cpp
@@ -7,6 +7,164 @@
 
 #include "BitLogicNintendo2.h"
 
-int main(int argc, char** argv) {
+
+std::unordered_set<bitset < SZVEC >> solveIt(bitset<SZVEC> input, bitset<SZVEC> hypoth, int size, int depth) {
+    std::unordered_set<bitset < SZVEC >> res;
+    int magic[] = {0, size / 2 - 1, size / 2, size - 1};
     
+    bool activCond=false;
+    bool activate_formul_debug=false;
+    unsigned long valHypActDebug=41;
+    
+    if(activCond)if(depth==0) activate_formul_debug=true;
+
+    if(activate_formul_debug)
+      cout << "hyp " << toStringBs(hypoth, size) << " deep= " << depth<<" ";
+    //  cout <<"magic ";for(int i=0;i<4;i++)cout << magic[i] ;cout << endl;
+    //if( input!=40 || depth==0 )cout <<"input " <<toStringBs(input, size) << "--- depth " << depth << endl;
+
+
+    for (unsigned int i = 0; i < 16; i++) {
+        bitset<SZVEC> v;
+        v[magic[0] + depth] = i & 1;
+        v[magic[1] - depth] = ((i >> 1) & 1);
+        v[magic[2] + depth] = ((i >> 2) & 1);
+        v[magic[3] - depth] = ((i >> 3) & 1);
+
+        //  cout<< "v(b hyp) : " << toStringBs(v, size) << endl;
+
+        for (int i = 0; i < depth; i++) {
+            v[magic[0] + i] = hypoth[magic[0] + i];
+            v[magic[1] - i] = hypoth[magic[1] - i];
+            v[magic[2] + i] = hypoth[magic[2] + i];
+            v[magic[3] - i] = hypoth[magic[3] - i];
+        }
+
+       if(activCond) if(depth>=1 && v==valHypActDebug) activate_formul_debug=true;
+            if(activate_formul_debug)
+          cout<< "--- depth " << depth << " v(aft hyp) : " << toStringBs(v, size) << endl;        
+
+        // check formula .... left
+        bool ext = input[0 + depth];
+        bool possible = false;
+         if(activate_formul_debug)    cout<<"{";
+        for (int i = 0; i < depth + 1; i++) {
+            int h = size / 2;
+            int riand = i;
+            int rilef = h + (depth - i);
+            possible = possible^(v[riand] & v[rilef]);
+              if(activate_formul_debug)       cout<<"("<<(riand)<<"&"<<(rilef)<<")"<< v[riand] << "&" <<  v[rilef];
+        }
+        if(activate_formul_debug)     cout<<"}"<<endl;
+         // cout << "ext " << ext << " calc "<< possible << endl;
+        bool rightFormulaIsPassed = false;
+        
+         if (depth >= (size / 2) - 1) {
+             rightFormulaIsPassed=(ext == possible);
+         }else        
+        if (ext == possible) {
+            // check formula .... right
+             if(activate_formul_debug)        cout << " check right formula part inputIs(" << toStringBs(input, size)  << ") " << endl;
+            int rightEdge = size - 1;
+            ext = input[size - 2 - depth]; // cout << "Extern bit " << " (" <<(size-2-depth) <<") is " << input[size-2-depth] <<endl;
+            possible = false;
+              if(activate_formul_debug)       cout<<"right {";
+            for (int i = 0; i < depth + 1; i++) {
+                int h = size / 2;
+                int riand = h - i - 1;
+                int rilef = rightEdge - depth + i;
+                possible = possible^(v[riand] & v[rilef]);
+             if(activate_formul_debug)         cout<<"("<<(riand)<<"&"<<(rilef)<<")"<< v[riand] << "&" <<  v[rilef];
+            }
+             if(activate_formul_debug)     cout<<"}"<<endl; 
+
+              // cout << "right form " <<" ext "<< ext << " possible "  << possible << endl;
+
+            rightFormulaIsPassed = (ext == possible);
+            //  cout << "righ formula is passed " << rightFormulaIsPassed << endl;
+        }
+
+
+        if (rightFormulaIsPassed) {
+
+            if (depth >= (size / 2) - 1) {
+                //   cout << " adding validated " <<  toStringBs(v, size) << endl;
+
+                res.insert(v);
+
+            } else {
+                std::unordered_set<bitset < SZVEC >> sub = solveIt(input, v, size, depth + 1);
+              if(activate_formul_debug)            cout << "++++++++ back to "<< depth << " " << "hyp " << toStringBs(hypoth, size) << " deep= " << depth<<" " <<endl;
+                for (const auto& subv : sub) {
+                    res.insert(subv);
+                    /* ... process elem ... */
+                }
+            }
+        }
+    } // fin parcours hypotheses
+
+    return res;
+}
+
+
+void testAndValidateMyInvertingAtRandomWithManualSize(){
+    
+    bool showResult=true;
+    
+    cout << "testAndValidateMyInvertingAtRandomWithManualSize "  << endl;
+    std::unordered_set<bitset < SZVEC >> done;
+    srand (0x478754);
+    bitset<SZVEC> my;
+    int sz = 16;
+    unsigned int val = 0x0e24;
+    //cout << formula(size/2) << endl;         
+    //cout << formula(size) << endl;     
+    //cout << formula(size*2) << endl;
+
+    int nbNumberToTry=10000;
+    long maxValue=1;
+    for(int i=0;i<sz-1;i++){
+        maxValue|= maxValue<<1;
+    }
+    
+    cout << "MaxValue " << maxValue << endl;
+    for (int i = 0; i < nbNumberToTry; i++) {
+        long rl=rand()^(((long)rand()) << 32);
+        rl=rl%maxValue;
+        //cout << " Random " << hex << rl << endl;
+        my = rl;
+
+        cout << toStringBs(my, sz) << " -> ";
+        cout << toStringBs(applyDirectFun(my, sz), sz) << endl;
+        bitset<SZVEC> myori=my;
+        my=applyDirectFun(my, sz);
+        
+        if(done.find(my)==done.end()){
+            done.insert(my);
+
+                cout << " inverting " << toStringBs(myori, sz) ;
+                 clock_t begin = clock();
+                std::unordered_set<bitset < SZVEC >> res = solveIt(my, my, sz, 0);
+                {
+                    int elemind = 0;
+                   if(showResult) for (const auto& elem : res) {
+                        cout << (elemind++) << " : " << toStringBs(elem, sz) << endl;
+                    }
+                }   
+                clock_t end = clock();
+                double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;   
+                cout << "  { exec time " << elapsed_secs << " s " << endl;
+                if(res.find(myori)==res.end()){
+                    cout << " ERROR " << toStringBs(myori,sz) << " not found " << endl;
+                }
+        }
+ 
+    }
+    cout << "----------- END OF testAndValidateMyInvertingAtRandomWithManualSize --------" << endl;
+
+  
+}
+
+int main(int argc, char** argv) {
+    testAndValidateMyInvertingAtRandomWithManualSize();
 }
